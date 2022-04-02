acd39d82cb65bf8704218327eba9bd2adaaec260
Prerit Das <preritdas@gmail.com>
2022-04-02 09:36:11 -0700

    Gave text_me function a docstring.

--- texts.py
+++ texts.py
@@ -14,6 +14,14 @@ nexmo_client = nexmo.Client(
 sms = nexmo.Sms(client = nexmo_client)
 
 def text_me(*messages: str):
+    """
+    Concatenates all given arguments. Doesn't add spaces between them.
+    Sends the message using the Nexmo account and user phone number
+    given in the _keys.py module.
+    
+    Returns True if the first (usually only) message sent had successful
+    delivery. Otherwise, returns False.
+    """
     # Create the message
 
     text_content = ''
