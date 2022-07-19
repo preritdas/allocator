3ee7757887d9431b9e380ffd657594019d08f382
Prerit Das <preritdas@gmai.com>
2022-07-19 18:34:57 -0400

    Built and tested rebalancing protocol.

--- rebalancing.py
+++ rebalancing.py
@@ -1,51 +1,115 @@
 # Non-local imports
+from tempfile import TemporaryFile
+import alpaca_trade_api as alpaca_api
 
-etf_allocation = {etf: alloc for etf in etfs.values() for alloc in allocation.values()}
+# Local imports
+import configparser
+import threading
+import time
+
+# Project modules
+import _keys
+import utils
+import errors
+from allocation import allocation
+
+
+# Instantiate Alpaca API
+alpaca = alpaca_api.REST(
+    key_id = _keys.Alpaca.API_KEY,
+    secret_key = _keys.Alpaca.API_SECRET,
+    base_url = _keys.Alpaca.BASE_URL
+)
+
+
+# Configs
+class Config:
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+
+    rebalance_threshold: float = float(config["Rebalancing"]["rebalance_threshold"])
+
+
+# Define an ETFs dictionary with ideal allocations by symbol
+etf_allocations = {value[1]: value[0] for value in allocation.values()}
+
+
+def _fractional_order_errorhandling(side: str, symbol: str, amount: float):
+    """Calls `utils.fractional_order` but handles errors if they come up.
+    This was designed for use in rebalancing orders, to skip a symbol if 
+    there isn't enough buying power due to timing or other reason."""
+    try:
+        utils.fractional_order(side, symbol, amount)
+    except alpaca_api.rest.APIError as e:
+        if str(e) == 'insufficient buying power':
+            errors.report_error(
+                f"Error rebalancing {symbol} with {side} order for ${amount}. Skipped."
+            )
+        else:
+            raise alpaca_api.rest.APIError(e)
 
 
 def _current_positions() -> dict[str, float]:
-    return {etf: float(alpaca.get_position(etf).market_value) for etf in etfs.values()}
+    """Returns a dictionary with keys being the symbol name and values
+    being a float of the market value of the position in the account.
+    Only searches for symbols mentioned in the allocation."""
+    etfs: list[str] = [value[1] for value in allocation.values()]
+    return {etf: float(alpaca.get_position(etf).market_value) for etf in etfs}
 
 
 def positional_deltas() -> dict[str, float]:
-    account_total = float(alpaca.get_account().equity)
+    """Returns a dict of how off each position is in the account based on the
+    ideal etf allocations. A negative value means the account is _under_ the
+    expectation, so more of it should be bought to compensate."""
+    account_total = utils.account_equity()
     positions = _current_positions()
 
     ideal_alloc = {}
-    for etf, alloc, in zip(etfs.values(), allocation.values()):
+    for etf, alloc in etf_allocations.items():
         ideal_alloc[etf] = alloc * account_total
 
     return_deltas = {}
     for position in positions:
-        return_deltas[position] = round((positions[position] - ideal_alloc[position]), 2)
+        delta = round((positions[position] - ideal_alloc[position]), 2)
+        
+        # Only if above threshold and if notional value is more than 2
+        if(
+            abs(delta) / ideal_alloc[position] > Config.rebalance_threshold
+            and abs(delta) > 2
+        ):
+            return_deltas[position] = delta
 
     return return_deltas
 
 
-def correct_deltas() -> None:
-    """Places orders."""
+def rebalance_portfolio() -> dict[str, float]:
+    """
+    Places orders and returns deltas.
+    
+    `'buy if delta < 0` because if `delta < 0` it means the account is _under_ 
+    the ideal allocation. 
+    """
     deltas = positional_deltas()
 
-    for position in deltas:
-        if deltas[position] >= 5:
-            print("")
-            print(position, deltas[position])
-            print(f"Selling {deltas[position]} dollars of {position}.")
-
-            # Do it
-            alpaca.submit_order(
-                symbol = position,
-                side = 'sell',
-                notional = deltas[position]
+    # Sell orders first to free up buying power
+    for position, delta in deltas.items():
+        if delta > 0:
+            thread = threading.Thread(
+                target = _fractional_order_errorhandling,
+                args = ('sell', position, abs(delta))
             )
-        elif deltas[position] < -5:
-            print("")
-            print(position, deltas[position])
-            print(f"Buying {deltas[position]} dollars of {position}.")
+            thread.start()
 
-            # Do it
-            alpaca.submit_order(
-                symbol = position,
-                side = 'buy',
-                notional = deltas[position] * -1
+    # Sleep to ensure execution
+    time.sleep(5)
+
+    # Buy orders next
+    for position, delta in deltas.items():
+        if delta < 0:
+            thread = threading.Thread(
+                target = _fractional_order_errorhandling,
+                args = ('buy', position, abs(delta))
             )
+            thread.start()
+
+    return deltas
