c63f258c3c93769874537233bfa9f489cfaeea97
Prerit Das <preritdas@gmai.com>
2022-07-19 18:37:07 -0400

    Add new rebalancing feature. Alert if program crashes.

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ import time
 
 # Project modules
 import allocation
+import rebalancing
 import utils
+import errors
 
 
 def main() -> None:
     """Executes `buy_assets` every day 10 minutes before market close."""
     while True:
         if utils.market_open():
+            rebalancing.rebalance_portfolio()
             allocation.allocate()
             time.sleep(86400)
         else:
@@ -17,4 +20,7 @@ def main() -> None:
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except Exception as e:
+        errors.report_error(f"Program crashed. Error: {str(e)}.")
