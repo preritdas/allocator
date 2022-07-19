cbbd01c5c4eb8448769412d576ae468114c3e95d
Prerit Das <preritdas@gmai.com>
2022-07-19 17:29:53 -0400

    Sample _keys.py for repo.

---
+++ _keys (sample).py
@@ -0,0 +1,20 @@
+class Alpaca:
+    API_KEY = ''
+    API_SECRET = ''
+    BASE_URL = ''
+    
+
+class Nexmo:
+    api_key = ''
+    api_secret = ''
+    sender: str = ''
+
+
+class Gmail:
+    email_address = '' 
+    password = ''
+
+
+class User:
+    phone_number: str = ''
+    email_address = ''
