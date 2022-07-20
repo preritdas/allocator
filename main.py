6a47247dce83d7087c2051b38392c24b5858c116
Prerit Das <preritdas@gmai.com>
2022-07-19 22:35:34 -0400

    Write text/email delivery update mechanism.

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# Non-local imports
+import mypytoolkit as kit
+
 # Local imports
 import time
 
@@ -9,14 +12,36 @@ import utils
 import errors
 
 
-def deliver_update(rebalances: dict[str, float], allocations: dict[str, float]):
+def deliver_update(allocations: dict[str, float], rebalances: dict[str, float]) -> None:
     """Texts an update of all that was done. Eventually, this will use email."""
-    update = (
-        f"""
-        Rebalances attemped: {rebalances}
+    # If both dictionaries are empty
+    if not allocations and not rebalances:
+        update = "No actions were taken today."
+        delivery.text_me(update)
+        delivery.email_me(update, subject="Allocator Daily Report")
+        return
 
-        Allocations made: {allocations}
-        """
+    sector_from_symbol = {val[1]: key for key, val in allocation.allocation.items()}
+
+    allocations_str = ""
+    for symbol, amount in allocations.items():
+        allocations_str += f"Allocated ${amount} of cash to {sector_from_symbol[symbol]}.\n"
+
+    rebalances_str = ""
+    for symbol, delta in rebalances.items():
+        if delta < 0:
+            rebalances_str += f"Added ${abs(delta)} to {sector_from_symbol[symbol]}.\n"
+        elif delta > 0:
+            rebalances_str += f"Removed ${abs(delta)} from {sector_from_symbol[symbol]}.\n"
+
+    update = (
+        f"The following is a summary report of actions taken/attempted by Allocator "
+        f"today, {kit.today_date()}.\n\n"
+        "Allocations:\n"
+        f"{allocations_str}\n\n"
+        "Rebalances:\n"
+        f"{rebalances_str}"
+        ""
     )
 
     delivery.text_me(update)
@@ -27,9 +52,9 @@ def main() -> None:
     """Executes `buy_assets` every day 10 minutes before market close."""
     while True:
         if utils.market_open():
-            attempted_rebalances = rebalancing.rebalance_portfolio()
-            allocations = allocation.allocate()
-            deliver_update(attempted_rebalances, allocations)
+            rebalances = rebalancing.positional_deltas()
+            allocations = allocation.calculate_quantities()
+            deliver_update(allocations, rebalances)
             time.sleep(86400)
         else:
             time.sleep(60)
