c8002590bddfe5e97c565616af2b4a73475cebe4
Prerit Das <preritdas@gmai.com>
2022-07-29 18:14:12 -0400

    Link to help in createkeys script.

--- createkeys.sh
+++ createkeys.sh
@@ -2,3 +2,4 @@
 
 mv 'keys (sample).ini' 'keys.ini'
 echo "Created template keys.ini and removed the sample file."
+echo "See https://github.com/preritdas/allocator#keysini to understand how exactly to format keys.ini."
