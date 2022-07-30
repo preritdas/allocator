50cf0122086fcff3cac3229b1e52b0d9a21d58e8
Prerit Das <preritdas@gmail.com>
2022-07-30 17:23:11 -0400

    Create default config file as well.

--- configurate.sh
+++ configurate.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 mv 'keys (sample).ini' 'keys.ini'
-echo "Created template keys.ini and removed the sample file."
-echo "See https://github.com/preritdas/allocator#keysini to understand how exactly to format keys.ini."
+mv 'config (sample).ini' 'config.ini'
+echo "Created template config.ini and keys.ini. Removed the sample files."
+echo "See https://github.com/preritdas/allocator#keysini to understand how exactly to format each file."
